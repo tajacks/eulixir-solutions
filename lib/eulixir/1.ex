12e6d248b742fcb7379a7d1f80d87b4f02b4a3b4
tajacks <tom@tomjack.ca>
2023-01-20 22:22:18 -0500

    problem 1

--- lib/eulixir/1.ex
+++ lib/eulixir/1.ex
@@ -1,7 +1,12 @@
 defmodule Eulixir.Problem1 do
   @moduledoc "https://projecteuler.net/problem=1"
 
-  def solve() do
-  
+  def solve() do 
+    3..999
+    |> Enum.filter(&is_multiple/1)
+    |> Enum.sum()
   end
+
+
+  def is_multiple(n), do: rem(n, 3) == 0 || rem(n, 5) == 0
 end
