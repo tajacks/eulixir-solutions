b03969634988b30e3e619589a69edd5ccedb2391
tajacks <tom@tomjack.ca>
2023-01-20 22:37:25 -0500

    problem 7

---
+++ lib/eulixir/7.ex
@@ -0,0 +1,10 @@
+defmodule Eulixir.Problem7 do
+  @moduledoc "https://projecteuler.net/problem=7"
+
+  import Multitool.Numbers.Primes
+
+  def solve() do
+    # :) 
+    nth_prime(10001)
+  end
+end
